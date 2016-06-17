9bb4f40a7a9f6ea57b4b18039dc194d57e5f673a
Vance <vmorris@us.ibm.com>
2016-06-17 17:18:56 -0500

    Update quine.go
    Nice quine, just fixing it so it'll pass the gofmt tool!
    Your original program had the following differences:
    
    $ ./quine > q.out 
    $ diff q.out main.go 
    6c6
    < 	code :=`package main
    ---
    > 	code := `package main
    15c15
    < }
    \ No newline at end of file
    ---
    > }

--- quine.go
+++ quine.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	code :=`package main
+	code := `package main
 
 import "fmt"
 
 func main() {
-	code :=
-	fmt.Print(code[:50] + "\u0060" + code + "\u0060" + code[50:])
-}`
-	fmt.Print(code[:50] + "\u0060" + code + "\u0060" + code[50:])
+	code := 
+	fmt.Print(code[:51] + "\u0060" + code + "\u0060" + code[51:])
+}
+`
+	fmt.Print(code[:51] + "\u0060" + code + "\u0060" + code[51:])
 }
